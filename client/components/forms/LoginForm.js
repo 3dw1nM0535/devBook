c05cb62073882ed3d0bdc7c16197e65f289eed68
3dw1nM0535 <workockmoses@gmail.com>
2018-02-15 22:58:56 +0300

    implemented user login page

--- client/components/forms/LoginForm.js
+++ client/components/forms/LoginForm.js
@@ -1,20 +1,55 @@
 import React from "react";
 import { Form, Button } from "semantic-ui-react";
+import isEmail from "validator/lib/isEmail";
+import PropTypes from "prop-types";
+
+import InlineError from "../messages/InlineError";
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: {},
+      data: {
+        email: "",
+        password: "",
+      },
       loading: false,
       errors: {},
     };
   }
+
+  onChange = (e) => {
+    this.setState({
+      ...this.state,
+      data: { ...this.state.data, [e.target.name]: e.target.value },
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validate(this.state.data);
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
+      this.props.submit(this.state.data);
+    }
+  }
+
+  validate = (data) => {
+    const errors = {};
+
+    if (!data.email) errors.email = "Can't be Blank";
+    if (!isEmail(data.email)) errors.email = "Invalid Email";
+    if (!data.password) errors.password = "Can't be Blank";
+
+    return errors;
+  }
+
   render() {
     const { data, loading, errors } = this.state;
     return (
-        <Form size="small" unstackable={false} onSubmit={this.onSubmit} loading={loading}>
-          <Form.Field>
+        <Form size="tiny" unstackable={false} onSubmit={this.onSubmit} loading={loading}>
+          <Form.Field error={!!errors.email}>
             <label htmlFor="email">Email</label>
             <input
               type="email"
@@ -24,8 +59,9 @@ class LoginForm extends React.Component {
               onChange={this.onChange}
               value={data.email}
             />
+          { errors.email && <InlineError text={errors.email} /> }
           </Form.Field>
-          <Form.Field>
+          <Form.Field error={!!errors.password}>
             <label htmlFor="password">Password</label>
             <input
               type="password"
@@ -35,11 +71,16 @@ class LoginForm extends React.Component {
               onChange={this.onChange}
               value={data.password}
             />
+          { errors.password && <InlineError text={errors.password} /> }
           </Form.Field>
-          <Button primary>Login</Button>
+          <Button size="tiny" color="green">Login</Button>
         </Form>
-      );
+    );
   }
 }
+
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired,
+};
 
 export default LoginForm;
--- client/components/pages/LoginPage.js
+++ client/components/pages/LoginPage.js
@@ -2,13 +2,22 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 
 import LoginForm from "../forms/LoginForm";
+import { styles } from "../styles/styles";
 
-const LoginPage = () => (
-  <Grid centered stackable>
-    <Grid.Column width={7}>
-      <LoginForm />
-    </Grid.Column>
-  </Grid>
-);
+
+class LoginPage extends React.Component {
+
+  submit = data => console.log(data);
+
+  render() {
+    return (
+      <Grid style={styles.loginPage} centered stackable>
+        <Grid.Column width={7}>
+          <LoginForm submit={this.submit} />
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
 
 export default LoginPage;
