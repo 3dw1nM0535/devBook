c98e510bf3a89d2a6d364d99aa6de77933550215
3dw1nM0535 <workockmoses@gmail.com>
2018-02-24 19:33:58 +0300

    server-side company/organisation validation and authentication

---
+++ server/routes/User/auth.js
@@ -0,0 +1,34 @@
+// User sign authentication route
+
+import express from "express";
+import Attendee from "../../models/Attendee";
+
+import { sendConfirmationEmail } from "../../mailer/authMailer";
+import { parseError } from "../../utils/parseErrors";
+
+const router = express.Router();
+
+// Sign up route handler
+router.post("/", (req, res) => {
+  const {
+    email,
+    firstname,
+    lastname,
+    dob,
+    password,
+  } = req.body.data;
+  const user = new Attendee({
+    email,
+    firstname,
+    lastname,
+    dob,
+  });
+  user.setPassword(password);
+  user.setConfirmationToken();
+  user.save().then((userRecord) => {
+    sendConfirmationEmail(userRecord);
+    res.json({ user: userRecord });
+  }).catch(err => res.status(400).json({ errors: parseError(err.errors) }));
+});
+
+export default router;
---
+++ server/routes/User/users.js
@@ -0,0 +1,71 @@
+// User login route handler
+import express from "express";
+import jwt from "jsonwebtoken";
+
+import Attendee from "../../models/Attendee";
+import { sendResetPasswordEmail } from "../../mailer/authMailer";
+import privateKeys from "../../../config/private_keys";
+
+const router = express.Router();
+
+// User login route handler
+router.post("/", (req, res) => {
+  const { credentials } = req.body;
+  Attendee.findOne({ email: credentials.email }).then((user) => {
+    if (user && user.isValidPassword(credentials.password)) {
+      res.json({ user: user.toJSON() });
+    } else {
+      res.status(400).json({ errors: { global: "Invalid credentials" } });
+    }
+  });
+});
+
+// Email confirmation route handler
+router.post("/confirmation", (req, res) => {
+  Attendee.findOneAndUpdate({ confirmationToken: req.body.token }, { confirmed: true, confirmationToken: "" }, { new: true })
+    .then(user => user ? res.json({ user: user.toJSON() }) : res.status(400).json({}));
+});
+
+// Forgot password route handler
+router.post("/forgot-password", (req, res) => {
+  Attendee.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      sendResetPasswordEmail(user);
+      res.json({});
+    } else {
+      res.status(400).json({ errors: { global: "No user with such an Email" } });
+    }
+  });
+});
+
+// Validate token route handler
+router.post("/validate-token", (req, res) => {
+  jwt.verify(req.body.token, privateKeys.SECRET_KEY, (err) => {
+    if (err) {
+      res.status(401).json({});
+    } else {
+      res.json({});
+    }
+  });
+});
+
+// Reset password route handler
+router.post("/reset-password", (req, res) => {
+  const { newPassword, token } = req.body.data;
+  jwt.verify(token, privateKeys.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ errors: { global: "Invalid token" } });
+    } else {
+      User.findOne({ _id: decoded._id }).then((user) => {
+        if (user) {
+          user.setPassword(newPassword);
+          user.save().then(() => res.json({}));
+        } else {
+          res.status(404).json({ errors: { global: "Invalid token" } });
+        }
+      });
+    }
+  });
+});
+
+export default router;
