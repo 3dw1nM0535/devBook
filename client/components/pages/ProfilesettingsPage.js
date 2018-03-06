3f5d437783250b20d746e493dc16b610d381114d
3dw1nM0535 <workockmoses@gmail.com>
2018-03-06 19:45:11 +0300

    implement authenticated user profile settings page

--- client/components/pages/ProfilesettingsPage.js
+++ client/components/pages/ProfilesettingsPage.js
@@ -1,21 +1,47 @@
 import React from "react";
 import { Header, Grid } from "semantic-ui-react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import ProfileComponent from "../forms/ProfileComponent";
+import ProfileForm from "../forms/ProfileForm";
+import { fetchProfile, updateProfile } from "../../actions/authUser";
 
 class ProfilesettingsPage extends React.Component {
-  submit = data => console.log(data);
+  state = {
+    data: {},
+  }
+  componentDidMount = () => this.props.fetchProfile(this.props._id)
+    .then(user => this.setState({ data: user }));
+
+  submit = (data, file) => this.props.updateProfile(data, file).then(() => this.props.history.push("/profile"));
 
   render() {
+    const { data } = this.state;
+
     return (
       <Grid>
-        <Grid.Column>
+        <Grid.Column width={9}>
           <Header as="h1">Profile settings</Header>
-          <ProfileComponent submit={this.submit} />
+          <ProfileForm data={data} submit={this.submit} />
         </Grid.Column>
       </Grid>
     );
   }
 }
 
-export default ProfilesettingsPage;
+ProfilesettingsPage.propTypes = {
+  fetchProfile: PropTypes.func.isRequired,
+  _id: PropTypes.string.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+  updateProfile: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    _id: state.user._id,
+  };
+}
+
+export default connect(mapStateToProps, { fetchProfile, updateProfile })(ProfilesettingsPage);
