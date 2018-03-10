74bfb99163100a12b03c3cb4a645bac49217a85a
3dw1nM0535 <workockmoses@gmail.com>
2018-03-10 12:51:14 +0300

    redefined UI

--- client/components/forms/ForgotPasswordForm.js
+++ client/components/forms/ForgotPasswordForm.js
@@ -1,78 +1,82 @@
 import React from "react";
-import { Form, Button, Message } from "semantic-ui-react";
+import { Form, Button, Message, Card } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import isEmail from "validator/lib/isEmail";
 
 import InlineError from "../messages/InlineError";
 
 class ForgotPasswordForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: {
-        email: "",
-      },
-      loading: false,
-      errors: {},
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: {
+				email: "",
+			},
+			loading: false,
+			errors: {},
+		};
+	}
 
-  onChange = (e) => {
-    this.setState({
-      ...this.state,
-      data: { ...this.state.data, [e.target.name]: e.target.value },
-    });
-  }
+	onChange = (e) => {
+		this.setState({
+			...this.state,
+			data: { ...this.state.data, [e.target.name]: e.target.value },
+		});
+	}
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validate(this.state.data);
-    this.setState({ errors });
-    if (Object.keys(errors).length === 0) {
-      this.setState({ loading: true });
-      this.props.submit(this.state.data).catch(err =>
-        this.setState({ errors: err.response.data.errors, loading: false }));
-    }
-  }
+	onSubmit = (e) => {
+		e.preventDefault();
+		const errors = this.validate(this.state.data);
+		this.setState({ errors });
+		if (Object.keys(errors).length === 0) {
+			this.setState({ loading: true });
+			this.props.submit(this.state.data).catch(err =>
+				this.setState({ errors: err.response.data.errors, loading: false }));
+		}
+	}
 
-  validate = (data) => {
-    const errors = {};
+	validate = (data) => {
+		const errors = {};
 
-    if (!isEmail(data.email)) errors.email = "Invalid Email";
+		if (!isEmail(data.email)) errors.email = "Invalid Email";
 
-    return errors;
-  }
+		return errors;
+	}
 
-  render() {
-    const { data, errors, loading } = this.state;
+	render() {
+		const { data, errors, loading } = this.state;
 
-    return (
-      <Form onSubmit={this.onSubmit} loading={loading}>
-        { errors.global && (
-          <Message negative>
-            <p>{errors.global}</p>
-          </Message>
-        )}
-        <Form.Field error={!!errors.email}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Enter your Email address for password reset"
-            onChange={this.onChange}
-            value={data.email}
-          />
-          { errors.email && <InlineError text={errors.email} /> }
-        </Form.Field>
-        <Button color="green">Send Password Reset Email</Button>
-      </Form>
-    );
-  }
+		return (
+			<Card centered fluid>
+				<Card.Content>
+					<Form onSubmit={this.onSubmit} loading={loading}>
+						{ errors.global && (
+							<Message negative>
+								<p>{errors.global}</p>
+							</Message>
+						)}
+						<Form.Field error={!!errors.email}>
+							<label htmlFor="email">Email</label>
+							<input
+								type="email"
+								name="email"
+								id="email"
+								placeholder="Enter your Email address for password reset"
+								onChange={this.onChange}
+								value={data.email}
+							/>
+							{ errors.email && <InlineError text={errors.email} /> }
+						</Form.Field>
+						<Button color="green">Send Password Reset Email</Button>
+					</Form>
+				</Card.Content>
+			</Card>
+		);
+	}
 }
 
 ForgotPasswordForm.propTypes = {
-  submit: PropTypes.func.isRequired,
+	submit: PropTypes.func.isRequired,
 };
 
 export default ForgotPasswordForm;
--- client/components/forms/PasswordresetForm.js
+++ client/components/forms/PasswordresetForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Card } from "semantic-ui-react";
 import Validator from "validator";
 import PropTypes from "prop-types";
 
@@ -51,33 +51,35 @@ class PasswordresetForm extends React.Component {
     const { data, loading, errors } = this.state;
 
     return (
-      <Form onSubmit={this.onSubmit} unstackable={false} loading={loading}>
-        <Form.Field error={!!errors.newPassword}>
-          <label htmlFor="newPassword">New Password</label>
-          <input
-            type="password"
-            name="newPassword"
-            id="newPassword"
-            placeholder="New Password"
-            onChange={this.onChange}
-            value={data.newPassword}
-          />
-          { errors.newPassword && <InlineError text={errors.newPassword} /> }
-        </Form.Field>
-        <Form.Field error={!!errors.confirmNewPassword}>
-          <label htmlFor="confirmNewPassword">Confirm New Password</label>
-          <input
-            type="password"
-            name="confirmNewPassword"
-            id="confirmNewPassword"
-            placeholder="Confirm New Password"
-            onChange={this.onChange}
-            value={data.confirmNewPassword}
-          />
-          { errors.confirmNewPassword && <InlineError text={errors.confirmNewPassword} /> }
-        </Form.Field>
-        <Button color="green">Reset Password</Button>
-      </Form>
+      <Card centered fluid>
+      	<Form onSubmit={this.onSubmit} unstackable={false} loading={loading}>
+	        <Form.Field error={!!errors.newPassword}>
+	          <label htmlFor="newPassword">New Password</label>
+	          <input
+	            type="password"
+	            name="newPassword"
+	            id="newPassword"
+	            placeholder="New Password"
+	            onChange={this.onChange}
+	            value={data.newPassword}
+	          />
+	          { errors.newPassword && <InlineError text={errors.newPassword} /> }
+	        </Form.Field>
+	        <Form.Field error={!!errors.confirmNewPassword}>
+	          <label htmlFor="confirmNewPassword">Confirm New Password</label>
+	          <input
+	            type="password"
+	            name="confirmNewPassword"
+	            id="confirmNewPassword"
+	            placeholder="Confirm New Password"
+	            onChange={this.onChange}
+	            value={data.confirmNewPassword}
+	          />
+	          { errors.confirmNewPassword && <InlineError text={errors.confirmNewPassword} /> }
+	        </Form.Field>
+	        <Button color="green">Reset Password</Button>
+      	</Form>
+      </Card>
     );
   }
 }
