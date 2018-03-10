ec4d2284dc234b5367c8e2f33b33a8317cf6bb3f
3dw1nM0535 <workockmoses@gmail.com>
2018-03-11 01:32:34 +0300

    renamed UI segment padding class for uniqueness

--- client/components/pages/HomePage.js
+++ client/components/pages/HomePage.js
@@ -7,7 +7,7 @@ import "../styles/styles.css";
 const HomePage = () => (
   <Grid stackable columns={1}>
     <Grid.Column>
-      <Segment className="segment" textAlign="center" padded="very">
+      <Segment className="segment-padded" textAlign="center" padded="very">
         <Header as="h1">Event.com</Header>
         <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link>
       </Segment>
