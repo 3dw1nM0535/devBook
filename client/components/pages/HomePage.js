d35076e3fa2ad5d2403805de2c004e3fb1d2fc95
3dw1nM0535 <workockmoses@gmail.com>
2018-03-11 01:32:34 +0300

    redefined UI

--- client/components/pages/HomePage.js
+++ client/components/pages/HomePage.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Header, Grid, Segment } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
-import { styles } from "../styles/styles";
+import "../styles/styles.css";
 
 const HomePage = () => (
   <Grid stackable columns={1}>
     <Grid.Column>
-      <Segment textAlign="center" padded="very" style={styles.segment}>
+      <Segment className="segment" textAlign="center" padded="very">
         <Header as="h1">Event.com</Header>
         <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link>
       </Segment>
