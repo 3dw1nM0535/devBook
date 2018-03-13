db6e793436520ad63680255932c6fd6957541b6e
3dw1nM0535 <workockmoses@gmail.com>
2018-03-13 03:07:13 +0300

    removed file upload logic

--- client/reducers/rootReducer.js
+++ client/reducers/rootReducer.js
@@ -2,10 +2,8 @@ import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
 import user from "./user";
-import file from "./file";
 
 export default combineReducers({
   user,
-  file,
   routing: routerReducer,
 });
